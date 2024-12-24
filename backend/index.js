117cc457be169df850bf8c262e7098ccafb06412
Escobar Viveros Juan Pablo <juan.escobar.viveros@correounivalle.edu.co>
2024-12-23 21:29:08 -0500

    BD en la nube

--- backend/index.js
+++ backend/index.js
@@ -12,10 +12,13 @@ app.use(express.json());
 
 const pool = new Pool({
   user: process.env.PG_USER,
-  host: process.env.PG_HOST || 'db', 
+  host: process.env.PG_HOST,
   database: process.env.PG_DB,
   password: process.env.PG_PASSWORD,
-  port: process.env.PG_PORT || 5432,
+  port: process.env.PG_PORT,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 
